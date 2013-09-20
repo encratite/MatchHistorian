0aa9f44cc8f57199b087404dc95684baed33475b
void <git.email.address@googlemail.com>
2013-09-20 06:14:47 +0200

    Added a configuration file

--- Source/matchHistorian/MatchHistorian.java
+++ Source/matchHistorian/MatchHistorian.java
@@ -1,10 +1,34 @@
 package matchHistorian;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class MatchHistorian {
+	final static String configurationFile = "MatchHistorian.properties";
+	
+	public MatchHistorian() {
+		
+	}
+	
+	static void writeConfiguration() {
+		try {
+    		Properties properties = new Properties();
+    		properties.setProperty("databaseUser", "user");
+    		properties.setProperty("database", "match_historian");
+    		properties.setProperty("webSocketPort", "81");
+    		properties.store(new FileOutputStream(configurationFile), null);
+    	}
+    	catch (IOException exception) {
+    		exception.printStackTrace();
+        }
+	}
+	
 	public static void main(String[] arguments) {
 		// RegexTest.runTest();
 		// SoupTest.runTest();
 		// PostgreSQLTest.runTest();
-		TestServer.runTest();
+		// TestServer.runTest();
+		writeConfiguration();
 	}
 }
